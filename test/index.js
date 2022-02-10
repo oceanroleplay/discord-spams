de03e45a7385b1f7beb51824dd8cb5dadd0e98d6
oceanroleplay <indianoceanroleplay@gmail.com>
2022-02-10 13:45:24 +0530

    chore: format

--- test/index.js
+++ test/index.js
@@ -1,9 +1,13 @@
 import { SpamMeta } from "../src/index.js";
 async function main() {
-    await SpamMeta.refreshMasterList();
-    if (SpamMeta.isSpam("if you need cars, goto https://disocrds.gift/NverABbCacD, get a free car now")) {
-        console.log("given message contain spam link");
-    }
+  await SpamMeta.refreshMasterList();
+  if (
+    SpamMeta.isSpam(
+      "if you need cars, goto https://disocrds.gift/NverABbCacD, get a free car now"
+    )
+  ) {
+    console.log("given message contain spam link");
+  }
 }
 main();
 //# sourceMappingURL=index.js.map
